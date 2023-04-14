b020a8125c1a0c054d4fe9612b8ea3bfc83fbe5d
Faris Basha <fbashatm@gmail.com>
2023-04-14 23:29:31 +0530

    updated languages

--- lib/controller/api.dart
+++ lib/controller/api.dart
@@ -1,23 +1,45 @@
 import 'package:dio/dio.dart';
 
+const API_KEY = "13a9817527mshaaf533c5b5d1a7bp1ee76djsn29f4b5a5792f";
+const headers = {
+  'X-RapidAPI-Key': API_KEY, //! API KEY HERE
+  'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com',
+  'content-type': 'application/x-www-form-urlencoded',
+};
+
+final _dio = Dio(
+  BaseOptions(
+    baseUrl: 'https://google-translate1.p.rapidapi.com',
+    headers: headers,
+  ),
+);
+
 class Translate {
+  static Future<List<String>> getLanguages() async {
+    try {
+      final _response = await _dio.get(
+        '/language/translate/v2/languages',
+      );
+
+      if (_response.statusCode == 200) {
+        final _data = _response.data;
+        return _data['data']['languages']
+            .map<String>((e) => e['language'].toString())
+            .toList();
+      } else {
+        throw Exception('Failed To Get Languages');
+      }
+    } on DioError catch (e) {
+      throw Exception(e.message);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
   static Future<String> getdata(
       {required String text,
       required String targetLang,
       String? sourceLang}) async {
-    var headers = {
-      'X-RapidAPI-Key': 'API_KEY!!!', //! API KEY HERE
-      'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com',
-      'content-type': 'application/x-www-form-urlencoded',
-    };
-
-    final _dio = Dio(
-      BaseOptions(
-        baseUrl: 'https://google-translate1.p.rapidapi.com',
-        headers: headers,
-      ),
-    );
-
     try {
       final _response = await _dio.post(
         '/language/translate/v2',
--- lib/view/second.dart
+++ lib/view/second.dart
@@ -7,22 +7,51 @@ class TranslatePage extends StatefulWidget {
 }
 
 class _TranslatePageState extends State<TranslatePage> {
+  @override
+  void initState() {
+    Translate.getLanguages().then((value) {
+      setState(() {
+        _languages = value;
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
+
+  var _isLoading = true;
+  var _languages = <String>[];
+
   TextEditingController _textEditingController1 = TextEditingController();
   TextEditingController _textEditingController2 = TextEditingController();
 
   String _selectedLanguage1 = "es";
   String _selectedLanguage2 = "en";
 
-  void _showBottomSheet() {
+  void _showBottomSheet(Function onPressed) async {
     showModalBottomSheet(
         context: context,
         builder: (BuildContext context) {
           return Container(
-            height: 300,
-            child: Center(
-              child: Text("Bottom sheet content goes here"),
-            ),
-          );
+              height: 300,
+              child: _isLoading
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : SingleChildScrollView(
+                      child: Column(children: [
+                        ..._languages
+                            .map((e) => Card(
+                                  child: TextButton(
+                                    onPressed: () {
+                                      onPressed(e);
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text(e),
+                                  ),
+                                ))
+                            .toList(),
+                      ]),
+                    ));
         });
   }
 
@@ -34,49 +63,106 @@ class _TranslatePageState extends State<TranslatePage> {
       ),
       body: Column(
         children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Text("Language 1: $_selectedLanguage1"),
+                      IconButton(
+                        onPressed: () {
+                          _showBottomSheet((value) {
+                            setState(() {
+                              _selectedLanguage1 = value;
+                            });
+                          });
+                        },
+                        icon: Icon(Icons.arrow_drop_down),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text("Language 2: $_selectedLanguage2"),
+                      IconButton(
+                        onPressed: () {
+                          _showBottomSheet((value) {
+                            setState(() {
+                              _selectedLanguage2 = value;
+                            });
+                          });
+                        },
+                        icon: Icon(Icons.arrow_drop_down),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ElevatedButton(
                 onPressed: () async {
-                  _selectedLanguage1 = "es";
-                  _selectedLanguage2 = "en";
-
+                  setState(() {
+                    _isLoading = true;
+                  });
                   final text = await Translate.getdata(
                       text: _textEditingController1.text,
                       sourceLang: _selectedLanguage1,
                       targetLang: _selectedLanguage2);
 
                   _textEditingController2.text = text;
-                  setState(() {});
+                  setState(() {
+                    _isLoading = false;
+                  });
                 },
                 child: Text("Translate 1 to 2"),
               ),
               ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
                   setState(() {
-                    _selectedLanguage1 = "en";
-                    _selectedLanguage2 = "es";
+                    _isLoading = true;
                   });
-                  _showBottomSheet();
+                  final text = await Translate.getdata(
+                      text: _textEditingController2.text,
+                      sourceLang: _selectedLanguage2,
+                      targetLang: _selectedLanguage1);
+
+                  _textEditingController1.text = text;
+                  setState(() {
+                    _isLoading = false;
+                  });
                 },
                 child: Text("Translate 2 to 1"),
               ),
             ],
           ),
-          TextField(
-            controller: _textEditingController1,
-            decoration: InputDecoration(
-              labelText: "Text 1",
-            ),
-          ),
-          SizedBox(height: 10),
-          TextField(
-            controller: _textEditingController2,
-            decoration: InputDecoration(
-              labelText: "Text 2",
-            ),
-          ),
+          _isLoading
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Column(
+                  children: [
+                    TextField(
+                      controller: _textEditingController1,
+                      decoration: InputDecoration(
+                        labelText: "Text 1",
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                      controller: _textEditingController2,
+                      decoration: InputDecoration(
+                        labelText: "Text 2",
+                      ),
+                    ),
+                  ],
+                ),
           SizedBox(height: 20),
         ],
       ),
